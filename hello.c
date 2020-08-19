4a44ee461878fe021f4c075e8aca70c347c7049a
ecorrado2 <ecorrado2@huskers.unl.edu>
2020-08-19 21:37:48 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Eric Corrado
+ * Date: 2020
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello World!\n");
+     
+     return 0;
+ }
